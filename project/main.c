71f24df606717ff06d5147ff4127f1561009da16
Aaren Flores <ajflores11@miners.utep.edu>
2024-12-06 23:21:39 -0700

    Buttons Play sound and Display the characters... but S2-S3 is buggy

--- project/main.c
+++ project/main.c
@@ -3,6 +3,10 @@
 #include "switches.h"
 #include "buzzer.h"
 #include "stateMachines.h"
+#include <libTimer.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
 
 void main(void){
   configureClocks();
@@ -10,6 +14,14 @@ void main(void){
   buzzer_init();
   enableWDTInterrupts();
 
+  lcd_init();
+  u_char width = screenWidth, height = screenHeight;
+
+  clearScreen(COLOR_BLACK);
+  int col = 0, row = 0;
+  int centerCol = width / 4, centerRow = height / 4;
+
+    
   or_sr(0x18); //CPU off,GIE on
 }
 //keep the red light on
--- project/stateMachines.c
+++ project/stateMachines.c
@@ -2,22 +2,30 @@
 #include "buzzer.h"
 #include "stateMachines.h"
 #include <msp430.h>
+#include "lcdutils.h"
+#include "lcddraw.h"
+
 
 
 void switch_state_advance(){
   switch (switch_state){
   case 1:
-    buzzer_set_period(1300); //buzzer is set to a input when a certain button is pressed
+    buzzer_set_period(1300);//buzzer is set to a input when a certain button is pressed
+    drawString5x7(0, 0, "Nice!", COLOR_WHITE, COLOR_BLACK);
     break;
   case 2:
     buzzer_set_period(200);
+    drawString5x7(0, 0, "You did it!", COLOR_FIREBRICK, COLOR_BLACK);
     break;
   case 3:
     buzzer_set_period(700);
+     drawString5x7(0, 0, "Congrats!", COLOR_SEA_GREEN, COLOR_BLACK);
     break;
   case 4:
     buzzer_off();
+    drawString5x7(0, 0, "Mute!", COLOR_YELLOW, COLOR_BLACK);
     break;
 
   }
+  switch_state = 0;
 }
